a6124cc6c0843d3d48561ad7477aa3119bca5534
Kostuyra <gkv110592@yandex.ru>
2023-05-20 12:46:17 +0300

    Initial commit

---
+++ src/main/java/com/example/homework210/EmployeeServiceInterface.java
@@ -0,0 +1,16 @@
+package com.example.homework210;
+import java.util.List;
+
+
+
+public interface EmployeeServiceInterface {
+
+	Employee addEmployee(String firstName, String lastName, Integer partOffice, Double salary);
+
+	Employee removeEmployee(String firstName, String lastName, Integer partOffice, Double salary);
+
+	Employee findEmployee(String firstName, String lastName, Integer partOffice, Double salary);
+
+	List<Employee> allEmployees();
+
+}
